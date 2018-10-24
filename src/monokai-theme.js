16c54b91664c4685e781f87b173b941c34d946bd
Bryce Kalow <bryce.kalow@target.com>
2018-10-23 21:15:26 -0500

    feat: styling, move slides around

---
+++ src/monokai-theme.js
@@ -0,0 +1,63 @@
+// @flow
+// Converted automatically using ./tools/themeFromVsCode
+
+/*:: import type { PrismTheme } from '../src/types' */
+
+var theme /*: PrismTheme */ = {
+  plain: {
+    color: '#f8f8f2',
+    backgroundColor: 'transparent'
+  },
+  styles: [
+    {
+      types: ['keyword'],
+      style: {
+        color: '#66d9ef'
+      }
+    },
+    {
+      types: ['comment'],
+      style: {
+        color: 'slategray'
+      }
+    },
+    {
+      types: ['string', 'changed'],
+      style: {
+        color: '#a6e22e'
+      }
+    },
+    {
+      types: ['punctuation', 'deleted'],
+      style: {
+        color: '#f8f8f2'
+      }
+    },
+    {
+      types: ['property', 'name', 'constant', 'symbol', 'plain'],
+      style: {
+        color: '#f92672'
+      }
+    },
+    {
+      types: ['number', 'builtin'],
+      style: {
+        color: '#a6e22e'
+      }
+    },
+    {
+      types: ['variable'],
+      style: {
+        color: '#f8f8f2'
+      }
+    },
+    {
+      types: ['function', 'attr-name', 'inserted'],
+      style: {
+        color: '#e6db74'
+      }
+    }
+  ]
+};
+
+module.exports = theme;
